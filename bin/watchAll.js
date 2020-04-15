d6b551e68d7e416e54a989c0131a9d80ef3ac543
Oli Oskarsson <oli@oskarsson.nl>
2020-04-15 10:49:11 +0200

    Switched to new npm structure with TypeScript support

---
+++ bin/watchAll.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+// Command line example:
+// "watch": "concurrently --names \"JSDOC, TSC, ROLLUP\" -c \"black.bold.bgBlue,black.bold.bgMagenta,black.bold.bgYellow\" \"npm:watch:docs\" \"npm:watch:tsc\" \"npm:watch:rollup\""
+
+process.env.DEV_WATCH_MODE = true
+
+const concurrently = require('concurrently')
+
+const buildDev = async () => {
+  return await concurrently(
+    [
+      {
+        command: 'yarn:watch-tsc',
+        name: 'TSC',
+        prefixColor: 'black.bold.bgMagenta'
+      },
+      {
+        command: 'yarn:watch-rollup',
+        name: 'ROLLUP',
+        prefixColor: 'black.bold.bgCyan'
+      },
+      {
+        command: 'yarn:watch-docs',
+        name: 'DOCS',
+        prefixColor: 'black.bold.bgYellow'
+      }
+    ],
+    {
+      killOthers: ['failure'],
+      restartTries: 1
+    }
+  )
+    .catch(err => Promise.resolve(err))
+}
+
+try {
+  buildDev()
+}
+catch(err){
+  console.log(err)
+}
